f20f96df0bf065c7344c9244229b277875809505
freddyflorial@gmail.com <freddyflorial@gmail.com>
2020-11-30 08:28:43 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "Black", "Cuddly", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "Brown", "Crazy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Grizzly", "Kind", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sargeant Brown", 7, "M", "Brown", "Angry", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "Black", "Loud", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Grizzly", "Loving", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "Black", "Sluggish", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, "M", "Grizzly", "Hungry", 0);
